f4c5f671e9a957e8b1c21077457060ec73271467
Priyankar Nigam <82797160+friday006@users.noreply.github.com>
2024-09-08 20:21:15 +0530

    Update apiCalls.js

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -4,7 +4,7 @@ export const loginCall = async (userCredential, dispatch) => {
   const API_URL = process.env.REACT_APP_API_URL;
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post(API_URL+"/auth/login", userCredential);
+    const res = await axios.post("https://social-backend.netlify.app/api/auth/login", userCredential);
 
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
